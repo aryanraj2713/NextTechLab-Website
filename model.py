f84183dc4ab24422d1d7c3360e71f3693859793d
Pranav Gupta <88652820+pranavgupta2603@users.noreply.github.com>
2022-03-01 18:44:23 +0530

    Updated Model

--- model.py
+++ model.py
@@ -4,8 +4,6 @@ from tensorflow.keras.models import load_model
 
 left, right, up, down= np.load('left.npy'), np.load('right.npy'), np.load('up.npy'), np.load('down.npy')
 
-#print(len(left) + len(right) + len(up) + len(down))
-
 y1 = np.full((1, len(left)), 0)
 y2 = np.full((1, len(right)), 1)
 y3 = np.full((1, len(up)), 2)
@@ -16,12 +14,9 @@ x = np.concatenate((left, right, up, down), axis = 0)
 y = np.concatenate((y1, y2, y3, y4), axis = 1)[0]
 
 arg = tf.reshape(x, (670, 8, 1))
-print(arg)
-
-
+                
 model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(8, input_shape=[8], activation='relu'),
-        #tf.keras.layers.Dense(, activation='relu'),
         tf.keras.layers.Dense(4, activation='softmax')
         ])
 
@@ -30,7 +25,7 @@ model.summary()
 try:
     model = load_model('model.h5')
 except:
-    history = model.fit(arg, y, epochs=500)
+    history = model.fit(arg, y, epochs=550)
     model.save("model.h5")
 
 
